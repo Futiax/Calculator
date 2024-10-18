b8653af6c5aa1c16390812d7a0b1961d42370f90
Nathanaël Buendia <futiax@gmail.com>
2024-10-18 19:15:41 +0200

    Repath the files

--- Calculator/script.js
+++ Calculator/script.js
@@ -2,13 +2,16 @@ async function calculateGold() {
     const playerTag = document.getElementById('playerTag').value.replace('#', '');
     const resultElement = document.getElementById('result');
     try {
-        const response = await fetch(`http://82.64.36.59:8182/players/%23${playerTag}`)
+        const response = await fetch(`http://yux.freeboxos.fr:8182/players/%23${playerTag}`)
         // Afficher la réponse brute dans la console pour le débogage
         console.log(response);
         const data = await response.json();
         const brawlers = data.brawlers;
         const or_costs = [0, 20, 35, 75, 140, 290, 480, 800, 1250, 1875, 2800];
         let totalGoldNeeded = 0;
+        let ga = 0
+        let ge = 0
+        let sp = 0
         const lvlmax = document.getElementById("levelSlider").value;
         const gamax = document.getElementById("gadgetsSlider").value;
         const spmax = document.getElementById("starPowersSlider").value;
@@ -22,14 +25,17 @@ async function calculateGold() {
             }
             console.log(`${brawler.name} ${brawler.gears.length} équipement(s) et ${brawler.gadgets.length} gadget(s) et ${brawler.starPowers.length} pouvoir(s) star`)
             // Vérifier les équipements
+            ge += Math.max(0,(gemax - brawler.gears.length))
             totalGoldNeeded += Math.max(0,(gemax - brawler.gears.length) * 1000);
             // Vérifier les gadgets
+            ga += Math.max(0,(gamax - brawler.gadgets.length))
             totalGoldNeeded += Math.max(0,(gamax - brawler.gadgets.length) * 1000);
             // Vérifier les star powers
             totalGoldNeeded += Math.max(0,(spmax - brawler.starPowers.length) * 2000);
+            sp += Math.max(0,(spmax - brawler.starPowers.length))
         });
 
-        resultElement.textContent = `Vous avez besoin de ${totalGoldNeeded} or pour maximiser votre compte, sois environ ${Math.round(totalGoldNeeded/49.8069)} prix star`;
+        resultElement.textContent = `Vous avez besoin de ${totalGoldNeeded} or pour votre objectif, sois environ ${Math.round(totalGoldNeeded/49.8069)} prix star. Il vous manque dans ce cadre : ${ge} Gears   ${ga} Gadgets   ${sp} Star powers`;
 
     } catch (error) {
         console.error('Erreur:', error);
